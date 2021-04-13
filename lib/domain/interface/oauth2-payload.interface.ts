eb5e023af83e41949cb6553f8fb5f597561e4285
Dmitriy <60390742+nik-clockwise@users.noreply.github.com>
2021-04-13 16:21:01 +0300

    Update oauth2-payload.interface.ts

--- lib/domain/interface/oauth2-payload.interface.ts
+++ lib/domain/interface/oauth2-payload.interface.ts
@@ -18,5 +18,5 @@ export interface Oauth2PayloadInterface {
     readonly accessToken: AccessTokenEntity;
 
     // The ID is common to all
-    readonly id:  number;
+    readonly id:  number|string;
 }
